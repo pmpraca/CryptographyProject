a3325ab8bcb1fdc3e2081fce10058f182d9c5a34
Pedro Praça <pedromesquitapraca@gmail.com>
2024-05-06 13:50:46 +0300

    Algorithm 100% done (TODO: capable of handling 1000 digits)

--- RSA/RSA.c
+++ RSA/RSA.c
@@ -17,12 +17,50 @@ int modExp(int base, int exponent, int modulus) {
     return result;
 }
 
+int modInv(int a, int m) {
+    int m0 = m, t, Q; // Q -> quocient
+    int x0 = 0, x1 = 1;
+
+    if (m == 1)
+        return 0;
+
+    // Apply extended Euclid Algorithm
+    while (a > 1) {
+
+        Q = a / m;
+
+        t = m;
+
+        // m is remainder now, process same as Euclid's algo
+        m = a % m, a = t;
+
+        t = x0;
+
+        x0 = x1 - Q * x0;
+
+        x1 = t;
+    }
+
+    // Make x1 positive
+    if (x1 < 0)
+        x1 += m0;
+
+    return x1;
+}
+
 // Function to encrypt a message
 int encrypt(int message) {
     return modExp(message, e, n); // send the pubKey(e,n)
 }
 
+/*
 // Function to decrypt a message
 int decrypt(int ciphertext) {
+    return modExp(ciphertext, d, n); // prKey(d)
+}*/
+
+// Function to decrypt a message
+int decrypt(int ciphertext) {
+    int d = modInv(e, r); // Calculate private exponent d
     return modExp(ciphertext, d, n); // prKey(d)
 }
--- RSA/RSA.h
+++ RSA/RSA.h
@@ -16,12 +16,12 @@
 
 #define r ((p - 1)*(q - 1))
 
-#define e   (3) // can be either 3,5,17 & 65537 the bigger the number the more secure it is
-#define d   (7)
+#define e (3) // can be either 3,5,17 & 65537 the bigger the number the more secure it is
 
 // Function to calculate a^b mod n  (a-> base, b-> exponent)
 int modExp(int base, int exponent, int modulus);
 int encrypt(int message);
 int decrypt(int ciphertext);
+
 
 #endif //CRYPTOGRAPHYPROJECT_RSA_H
